6213961284cb8d90a0f992db7c41c01af365e119
Michael Cramer <cramer@imgur.com>
2016-05-18 12:02:39 -0700

    bash options

--- samples/bash_options.sh
+++ samples/bash_options.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-this_dir=$(cd $(dirname $0); pwd -P)
+script_dir=$(cd $(dirname $0); pwd -P)
 
-getopts
+while getopts ":ab:" opt; do
+    case $opt in
+    a)
+        exec=echo
+		;;
+    b)
+        value=$OPTARG
+		;;
+    \?)
+        echo "Invalid option: -$opt" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$opt requires an argument." >&2
+        exit 1
+        ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
 echo "whoa..."
