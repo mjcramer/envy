ce3ff24200d7c4ad0087e740adf4783ba6f21164
Michael Cramer <michael@cramer.name>
2017-06-21 15:20:07 -0700

    added playbooks

--- bin/photo-rename.py
+++ bin/photo-rename.py
@@ -18,38 +18,61 @@ import math
 #         ret[decoded] = value
 #     return ret
 
+DEBUG = False
 
-def find_number(filename):
-  print(filename)
-  regex = re.compile("(.*)(\d*).(.*)", re.DEBUG if args.debug else 0)
-  (base, digit, extension) = regex.match(filename)
+# default_match='(.*?)0*(\d+)(\.\w*)'
+default_match = '(.*?)'
+
+total_count = 0
+
+class PhotoFileName:
+  def __init__(self, path, base, extension, number = None):
+    global total_count
+    self.path = path
+    self.base = base
+    self.extension = extension
+    if number:
+      self.number = number 
+    else:
+      total_count += 1
+      self.number = total_count
+    
+  def __to_str():
+    print("PhotoFileName: path={} base={} ext={} order={}".format(
+      self.path, self.base, self.extension, self.number if self.number else "NONE"
+    ))
+
+# def find_number(filename):
+#   print(filename)
+#   regex = re.compile("(.*)(\d*).(.*)", re.DEBUG if DEBUG else 0)
+#   (base, digit, extension) = regex.match(filename)
   # print(base, digit, extension)
 
-def collect_files(args): 
-  for folder in args.folders:
+def collect_files(folders):
+  base_extension_regex = re.compile("(.*)\.(\w*)", re.DEBUG if DEBUG else 0)
+  for folder in folders:
     if not os.path.isdir(folder):
         print('Input folder {} does not exist, skipping...'.format(folder))
         continue
     for (path, dirs, files) in os.walk(folder):
-      if args.debug:
-        print("Walking path: {} {} {}".format(path, dirs, files)) 
+      if DEBUG:
+        print("Walking path: {} {} {}".format(path, dirs, files))
       for file in files:
-        if args.debug:
-          print("Adding input file: {}/{}".format(path, file))
-        yield "{}/{}".format(path, file)
+        m = base_extension_regex.match(file)
+        if m:
+          full_path = "{}/{}.{}".format(path, m.group(1), m.group(2))
+          if DEBUG:
+            print("Adding input file: " + full_path)
+          yield [full_path, m.group(1), m.group(2)]
 
 
-def match_path(full_path, regex, base, order, ext):
-  d = os.path.dirname(full_path)
-  f = os.path.basename(full_path)
-  print("{} {}: {} {} {} {}".format(d, f, regex, base, order, ext))
-  
-  m = regex.match(f)
+def match_base(regex, path, name, ext):
+  if DEBUG:
+    print("regex={} path={} name={} ext={}".format(regex, path, name, ext))
+  m = regex.match(name)
   if m:
-    print("blah")
-    return [ full_path, m.group(base), m.group(order), m.group(ext) ]
+    return PhotoFileName(path, m.group(1), ext)
   else:
-    print("map to none")
     return None
 
 
@@ -73,7 +96,7 @@ def main():
     # Optional Args
     parser.add_argument('-D', '--debug', action='store_true', help='Display verbose debugging information about files and pattern matches.')
     # parser.add_argument('-m', '--match', type=str, default='(.*?)0*(\d+)(\.\w*)', metavar='PATTERN', help='Only rename files matching PATTERN. Note that this applies to the basename only and does not include the file extension.')
-    parser.add_argument('-m', '--match', type=str, default='(.*?)0*(\d+)(\.\w*)', metavar='PATTERN', help='Only rename files matching PATTERN. Note that this applies to the basename only and does not include the file extension.')
+    parser.add_argument('-m', '--match', type=str, default=default_match, metavar='PATTERN', help='Only rename files matching PATTERN. Note that this applies to the basename only and does not include the file extension.')
 
     parser.add_argument('-b', '--basename-field', type=int, default=1, metavar='BASENAME', help='The match group number in the regex pattern that matches the file\'s base name.')
     parser.add_argument('-o', '--order-field', type=int, default=2, metavar='ORDER', help='The match group number in the regex pattern that matches the file\'s order number.')
@@ -92,6 +115,8 @@ def main():
     args = parser.parse_args()
     if args.debug:
       print(args)
+      global DEBUG 
+      DEBUG = True
     
     if not os.access(args.directory, os.W_OK):
       print("Directory {} is not writeable.".format(args.directory))
@@ -100,31 +125,71 @@ def main():
     if args.debug:
       print("Mode is {}".format(args.mode))
 
-
     regex = re.compile(args.match, re.DEBUG if args.debug else 0)
     if args.debug:
       print("There are {:d} capture groups in this pattern: {}".format(regex.groups, args.match))
 
-    input_files = collect_files(args)
+    if regex.groups == 0:
+      base_group = 0
+      number_group = 0
+    elif regex.groups == 1:
+      if args.basename_field == 1:
+        base_group = 1
+        number_group = 0
+      elif args.order_field == 1:
+        base_group = 0
+        number_group = 1
+      else:
+        print("You must set group 1 field!")
+        sys.exit(2)
+    elif regex.groups == 2:
+      if args.basename_field == 1:
+        base_group = 1
+        number_group = 2
+      elif args.order_field == 1:
+        base_group = 2
+        number_group = 1
+      else:
+        print("You must set group 1 field!")
+        sys.exit(2)
+      
+    else:
+      print("Too many match groups in pattern: {}".format(regex.groups))
+      sys.exit(2)
+      
+    # for i in range(regex.groups):
+      # if args.basename_field == i+1:
+      #   group[i] =
+      # print(i)
+      
+    # fields = list(filter(lambda x: x>0, [ args.basename_field, args.order_field, args.extension_field ]))
+    # if len(fields) != regex.groups:
+    #   print("There must be a pattern group for each specified index")
+    #   sys.exit(2)
 
-    source_files = list(map(lambda x: match_path(x, regex, args.basename_field, args.order_field, args.extension_field), input_files))
-    print("{} source files".format(len(source_files)))
+    input_files = list(collect_files(args.folders))
+    if DEBUG:
+      print("{} collected files:".format(len(input_files)))
+      for f in input_files:
+        print(f)
+      
+    source_files = list(map(lambda x: match_base(regex, x[0], x[1], x[2]), input_files))
+    if DEBUG:
+      print("{} source files".format(len(source_files)))
+
     filtered_files = list(filter(None.__ne__, source_files))
-    print("{} filtered files".format(len(filtered_files)))
+    if DEBUG:
+      print("{} filtered files".format(len(filtered_files)))
     
     number = args.number
-    # padding = args.zero_padding if args.zero_padding else int(math.log10(len(source_files) + args.number)) + 1
-    padding = args.zero_padding if args.zero_padding else 2
-    for path, base, order, extension in filtered_files:
+    padding = args.zero_padding if args.zero_padding else int(math.log10(len(filtered_files) + args.number)) + 1
+    for name in filtered_files:
       # First check if it has an index number at the end
-      # ext = m.group(args.extension_field).lower()
-      print("dir, file, ext: {}, {}, {}".format(base, order, extension))
-      
-      target_file = ("{0}{1}{2:0" + str(padding) + "d}.{3}").format(args.replace, args.separator, number, extension)
+      target_file = ("{0}{1}{2:0" + str(padding) + "d}.{3}").format(args.replace, args.separator, name.number, name.extension.lower())
       if args.debug or args.test:
-        print("Replacing source file {} with {}".format(f, target_file))
+        print("Replacing source file {} with {}".format(name.path, target_file))
       if not args.test:
-        os.rename(path, target_file)
+        os.rename(name.path, target_file)
       number += 1
 
     parser.exit()
