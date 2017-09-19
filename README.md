7d592649e67f5327619b68f2f954ea1b9788f5c5
Michael Cramer <michael@cramer.name>
2017-09-19 12:55:16 -0700

    updates

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ envy
 ===
 
 A bunch of useful and useless scripts that I've collected over time...
+
+
+
+
+How To Set Up Environment
+===
+
+# Common Steps
+
+- Generate ssh key
+```ssh-keygen -t rsa```
+- Add ssh key to github
+- Clone this project
+
+
+
+# MacOS
+
+From App Store
+- SizeUp
+- Owly
--- bin/ansible-ssh-host.sh
+++ bin/ansible-ssh-host.sh
@@ -7,10 +7,11 @@ fi
 
 host=$(ansible --module-name=debug --args="msg={{ansible_host}}" --one-line $1 | sed -n 's/.* => \(.*\)/\1/p' | jq --raw-output .msg)
 user=$(ansible --module-name=debug --args="msg={{ansible_user}}" --one-line $1 | sed -n 's/.* => \(.*\)/\1/p' | jq --raw-output .msg)
-key=$(ansible --module-name=debug --args="msg={{ansible_ssh_private_key}}" --one-line $1 | sed -n 's/.* => \(.*\)/\1/p' | jq --raw-output .msg)
+key=$(ansible --module-name=debug --args="msg={{ansible_ssh_private_key_file}}" --one-line $1 | sed -n 's/.* => \(.*\)/\1/p' | jq --raw-output .msg)
 
 if [ -n "$key" ]; then
     ssh_opts="-i $key"
 fi
 
+echo ssh $ssh_opts $user@$host
 exec ssh $ssh_opts $user@$host
