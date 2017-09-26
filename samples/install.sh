9aea13b7a839d42c7802297af3f6a44ee3b72797
Michael Cramer <michael@cramer.name>
2017-09-26 13:02:04 -0700

    configuration updates

---
+++ samples/install.sh
@@ -0,0 +1,6 @@
+(which kubectl &>/dev/null) || die '* missing kubectl, is it installed?'
+(which bx &>/dev/null) || die '* missing bx, is it installed?'
+(which jq &>/dev/null) || die '* missing jq, is it installed?'
+(which docker &>/dev/null) || die '* missing docker; is it installed?'
+(which mvn &>/dev/null) || die '* missing mvn; is it installed?'
+
