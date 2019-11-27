d43bc0bfcfd6466fc687eb9c4201c6111faa15d9
Michael Cramer <michael@cramer.name>
2019-11-27 15:37:31 -0800

    refactor dotfiles

--- bin/colors-16.sh
+++ bin/colors-16.sh
@@ -9,25 +9,48 @@ fg_colors=$(seq 30 37)
 bg_colors=$(seq 40 47)
 style_codes=$(seq 0 4)
 
-echo -n "     "
+printf "\n             "
 for bg_color in $bg_colors; do 
-  printf "   %2s  " $bg_color
+  printf "   %2s   " $bg_color
 done 
-printf "\n    "
+printf "\n      ---------------"
 for bg_color in $bg_colors; do 
   echo -n "-------"
 done 
 printf "\n"
 
+printf "      | "
+for style_code in $style_codes; do
+  echo -en "\033[${style_code}m${style_code}\033[0m"
+done
+printf " "
+for bg_color in $bg_colors; do 
+  # printf " "
+  echo -en "\033[${bg_color}m \033[0m"
+  for style_code in $style_codes; do
+    echo -en "\033[${style_code};${bg_color}m${style_code}\033[0m"
+  done
+  echo -en "\033[${bg_color}m \033[0m"
+  printf " "
+done
+printf "\n"
 for fg_color in $fg_colors; do 
-  printf " %2s |" ${fg_color}
+  printf "  %2s  |" ${fg_color}
+  echo -en "\033[${fg_color}m \033[0m"
+  for style_code in $style_codes; do
+    echo -en "\033[${style_code};${fg_color}m${style_code}\033[0m"
+  done
+  echo -en "\033[${fg_color}m \033[0m"
+  # printf " "
   for bg_color in $bg_colors; do 
-    printf " "
+    # printf " "
+    echo -en "\033[${fg_color};${bg_color}m \033[0m"
     for style_code in $style_codes; do
       echo -en "\033[${style_code};${fg_color};${bg_color}m${style_code}\033[0m"
     done
+    echo -en "\033[${fg_color};${bg_color}m \033[0m"
     printf " "
   done
-  echo
+  printf "\n"
 done
-
+printf "\n"
