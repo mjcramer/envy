bb2f7d3c59775eeb1bc254d7967fc09747984e90
Michael Cramer <michael@cramer.name>
2019-11-27 16:16:29 -0800

    cleaned up powerline

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,4 @@
 
-powerline_root=(pip3 show powerline-status | sed -E -n 's/^Location: (.+)/\1/p')
+set powerline_root (pip3 show powerline-status | sed -E -n 's/^Location: (.+)/\1/p')
 set fish_function_path $fish_function_path "$powerline_root/powerline/bindings/fish"
 powerline-setup
