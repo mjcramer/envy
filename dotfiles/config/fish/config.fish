33a264bfbeff8b708ef18ba598f079a19e2fc6c7
Michael Cramer <michael@cramer.name>
2019-11-27 17:18:40 -0800

    fix virtualenv prompt

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,5 @@
 
+set VIRTUAL_ENV_DISABLE_PROMPT true
 set powerline_root (pip3 show powerline-status | sed -E -n 's/^Location: (.+)/\1/p')
 set fish_function_path $fish_function_path "$powerline_root/powerline/bindings/fish"
 powerline-setup
