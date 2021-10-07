157461b24f8a6f8a704a5a8667f41301901538d7
Michael Cramer <mjcramer@gmail.com>
2021-10-07 09:49:21 -0700

    update brew

---
+++ bin/kafka-dump-topic.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd $(dirname $0); pwd -P)
+
+A=false
+bootstrap_servers="localhost:9092"
+
+usage() {
+    echo "This script dumps everything from a kafka topic."
+    echo "Usage: ${0##*/} [flags] <parameter1> <parameter2> ..."
+    echo "  -h          Print usage instructions"
+    echo "  -a          Option A switch"
+    echo "  -b SERVERS  Bootstrap servers to kafka command"
+}
+
+while getopts ":hab:" opt; do
+    case $opt in
+    h)
+        usage
+        exit 0
+        ;;
+    a)
+        A=true
+        ;;
+    b)
+        bootstrap_servers=$OPTARG
+        ;;
+    \?)
+        echo "Invalid option: -$opt" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$opt requires an argument." >&2
+        exit 1
+        ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+if [ -z "$1" ]; then
+  echo "Please specify a topic to dump."
+  exit 1
+else
+  topic=$1
+fi
+
+kafkacat -b ${bootstrap_servers} -C -t ${topic} -o beginning -D "" -e | protoc --decode_raw
+
