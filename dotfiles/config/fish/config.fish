077d73d12142a58a8022a425aa2b562c95ab9be3
Michael Cramer <mjcramer@gmail.com>
2024-06-12 13:28:10 -0700

    updates

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,31 +1,37 @@
 
 # Set up fisher for installing fish packages
-#if not functions -q fisher
-#    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-#    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+if not functions -q fisher
+   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 #    fish -c fisher
-#end
+end
 
 # Set up tide prompt 
 set -g tide_left_prompt_items time pwd git 
 set -g tide_right_prompt_items status cmd_duration jobs virtual_env kubectl toolbox terraform aws 
 
 # Set up path
-set -Ua fish_user_paths /usr/local/bin /usr/local/sbin ~/envy/bin ~/go/bin 
+set -Ua fish_user_paths /usr/local/bin /usr/local/sbin ~/envy/bin ~/go/bin '/Applications/IntelliJ IDEA.app/Contents/MacOS' 
+
+# Set up homebrew
+if test -e /opt/homebrew/bin/brew 
+  eval (/opt/homebrew/bin/brew shellenv)
+end
 
 # Set up jenv for managing JDK paths
 if test (which jenv)
   status --is-interactive; and jenv init - | source
 end
 
-if test -e /usr/libexec/java_home
-  set -x JAVA_HOME (/usr/libexec/java_home)
-end
+# Need to figure out how to run commands to install a default java and add it to jenv,
+# otherwise it complains and uglies the shell
+# sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+# jenv add /opt/homebrew/opt/openjdk@17
 
-# Set up fnm for node management
-if test (which fnm) 
-  fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled | source
-end
+## Set up fnm for node management
+# if test (which fnm)
+#   fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled | source
+#   fnm completions --shell fish | source
+# end
 
 # Set up completions
 for command in "cr op" 
